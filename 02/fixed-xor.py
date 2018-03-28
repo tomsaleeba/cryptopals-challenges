bf99f081c32a1ad6896a993471241e93c7bd34b9
Tom Saleeba <tom.saleeba@gmail.com>
2018-03-28 13:20:10 +1030

    feat: solved challenge 2

---
+++ 02/fixed-xor.py
@@ -0,0 +1,16 @@
+# https://cryptopals.com/sets/1/challenges/2
+import sys
+import binascii
+src1_hex_bytes_text = sys.argv[1]
+src1_parsed_bytes = binascii.unhexlify(src1_hex_bytes_text)
+src2_hex_bytes_text = sys.argv[2]
+src2_parsed_bytes = binascii.unhexlify(src2_hex_bytes_text)
+byte_length = len(src1_parsed_bytes)
+out_bytes = bytearray(byte_length)
+for i in range(0, byte_length):
+  src1_curr = src1_parsed_bytes[i]
+  src2_curr = src2_parsed_bytes[i]
+  xor_result = src1_curr ^ src2_curr
+  out_bytes[i] = xor_result
+result = binascii.hexlify(out_bytes)
+print(result)
