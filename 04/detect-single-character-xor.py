6d291013c9fa44f3bcfbd33519cd588062df4ebe
Tom Saleeba <tom.saleeba@gmail.com>
2018-04-14 15:15:41 +0930

    fix: still not working. Verified that it *can* work when I feed in the cyphertext from challenge 3, just don't get anything good out for the input

--- 04/detect-single-character-xor.py
+++ 04/detect-single-character-xor.py
@@ -1,35 +1,26 @@
 #!/usr/bin/python3
-# https://cryptopals.com/sets/1/challenges/3
+# https://cryptopals.com/sets/1/challenges/4
 import sys
 import binascii
 import re
+import logging
 from functools import reduce
 
-global_bests = []
+logging.basicConfig()
+logger = logging.getLogger('dscxor')
+# logger.setLevel(logging.DEBUG)
+
 
 def process_line(line):
-    best_scoring_results = {
-        'score': 0,
-        'records': []
-    }
-
     def process(cypher_char, enc_msg_bytes):
-        src_parsed_bytes = enc_msg_bytes
-        # src_parsed_bytes = binascii.unhexlify(enc_msg_bytes)
-        # print(enc_msg_bytes[0])
-        # print(src_parsed_bytes[0])
-        byte_length = len(src_parsed_bytes)
+        byte_length = len(enc_msg_bytes)
         out_bytes = bytearray(byte_length)
         for i in range(0, byte_length):
-            src_curr = src_parsed_bytes[i]
+            src_curr = enc_msg_bytes[i]
             xor_result = src_curr ^ cypher_char
             out_bytes[i] = xor_result
-        try:
-            decoded_ascii = out_bytes.decode('utf-8')
-        except UnicodeDecodeError:
-            return {'score': 0, 'bytes': out_bytes}
-        total_length = len(decoded_ascii)
-        without_alphabet = re.sub(r'[A-Za-z\'\.]', '', decoded_ascii)
+        total_length = len(out_bytes)
+        without_alphabet = out_bytes.translate(None, b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
         letters_count = total_length - len(without_alphabet)
         return {
             'score': letters_count,
@@ -38,50 +29,45 @@ def process_line(line):
             'cypher_key': cypher_char
         }
 
-    possible_keys = list(range(ord('0'), ord('z') + 1))
+    best_scoring_results = {
+        'score': 0,
+        'records': []
+    }
+    possible_keys = list(range(ord('A'), ord('z') + 1))
     src_hex_bytes_text = line.strip()
+    src_parsed_bytes = binascii.unhexlify(src_hex_bytes_text)
+    logger.debug('Parsed bytes (len=%d): %s' % (len(src_parsed_bytes), src_parsed_bytes))
     for i in possible_keys:
-        r = process(i, src_hex_bytes_text)
+        r = process(i, src_parsed_bytes)
         curr_score = r['score']
+        if curr_score == 0:
+            continue
         if curr_score > best_scoring_results['score']:
             best_scoring_results['records'] = [r]
             best_scoring_results['score'] = curr_score
             continue
-        if curr_score == 0:
-            continue
         if curr_score == best_scoring_results['score']:
-            best_scoring_results['records'].append(r)
-    return best_scoring_results
+            best_scoring_results['records'] += [r]
+    return best_scoring_results['records']
 
+
+global_bests = []
 with open(sys.argv[1], 'rb') as f:
     for line in f:
+        logger.debug('Raw hex-text line: %s' % line)
         best_results = process_line(line)
-        if best_results['score'] > 0:
-            global_bests.append(best_results['records'][0])
-            if len(best_results['records']) > 1:
-                print('more than 1 best record, dumping')
-                for curr_best in best_results['records']:
-                    print('  %s' % curr_best['msg'])
-        # break # remove
+        global_bests += best_results
 
-print('Processed %d lines' % len(global_bests))
+logger.info('Qualifying decoded lines: %d' % len(global_bests))
 
-def highest_score(accum, curr):
-    # print(curr)
-    if curr['score'] >= accum['score']:
-        accum['score'] = curr['score']
-        accum['matches'] = [curr]
-    elif curr['score'] == accum['score']:
-        accum['matches'].append(curr)
-    return accum
-
-best_best = reduce(highest_score, global_bests, {
-    'score': 0,
-    'matches': []})
-for curr in best_best['matches']:
-    print('Best score: %d using key "%s" for input "%s" with result "%s"' % (
+def getkey(rec):
+    return rec['score']
+global_bests.sort(key=getkey)
+records_to_display=5
+for curr in global_bests[-1 * records_to_display:]:
+    print('score: %d using key "%s" for result "%s" with input "%s"' % (
             curr['score'],
             chr(curr['cypher_key']),
-            curr['input'],
             curr['msg'],
+            curr['input'],
         ))
